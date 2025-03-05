104ccc49476cbcfb144e0edcc748e67e7ffa3319
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-05 16:57:37 +0200

    Add first component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NewsItem from "./components/NewsItem/index.jsx";
+
 
 function App() {
+  const newItem ={
+    title: 'Some news',
+    body: 'About this news'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <NewsItem news={newItem}/>
   );
 }
 
 export default App;
+
---
+++ src/components/NewsItem/index.jsx
@@ -0,0 +1,10 @@
+
+function NewsItem(props){
+    const{news:{title,body}}=props
+    return <article>
+      <h2>{title}</h2>
+      <p>{body}</p>
+    </article>
+  }
+
+  export default NewsItem;
