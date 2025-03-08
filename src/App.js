448582f0d8b2f91dde81da0ea9d1779829cb8c44
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-08 22:48:24 +0200

    Add component ViewPortParams

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 
-import './App.css';
-import Counter from './components/Counter';
+import { Component } from "react";
+import ViewPortParams from "./components/ViewPortParams/ViewPortParams";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVisible: false,
+    };
+  }
+  handleClick=()=>{
+    const { isVisible } = this.state;
+    this.setState({
+      isVisible : !isVisible
 
-function App() {
-  return <Counter />
+    })
   }
 
-export default App;
 
-//new Counter().render()
+  render() {
+    const { isVisible } = this.state;
+    return(
+    <>
+    <button onClick={this.handleClick}>{isVisible ? 'Unmount' : 'Mount'}</button>
+    {isVisible&&<ViewPortParams/>}
+  </>
+  )
+  }
+}
+
+export default App;
