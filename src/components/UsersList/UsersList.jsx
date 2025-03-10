2dbc27679e7a0323dcb5b532989e2a9a74a62b27
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-10 23:22:31 +0200

    Add UsersList key

--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -1,22 +1,49 @@
-import React from 'react';
-import styles from'./UsersList.module.css'
+import React from "react";
+import styles from "./UsersList.module.css";
+
+const usersData = [
+  { id: 1, firstName: "Nataliia", lastName: "Yakovenko" },
+  { id: 2, firstName: "Roland", lastName: "Simonyan" },
+  { id: 3, firstName: "Den", lastName: "Braun" },
+];
 
 class UsersList extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        users:[1,2,3,4]
-
+      users: usersData.map((u) => ({ ...u, isSelected: false })),
     };
   }
 
+  selectUser = (id) => {
+    const { users } = this.state;
+    const findUser = users.findIndex((u) => {
+    return  u.id === id;
+    });
+    const newUsers = [...users];
+    newUsers[findUser] = {
+      ...users[findUser],
+      isSelected: !newUsers[findUser].isSelected,
+    };
+    this.setState({users:newUsers})
+  };
 
+
+  mapUsers = (u, id) => {
+    const inlineStyle = {
+      backgroundColor: u.isSelected ? "yellow" : "transparent",
+    };
+    return (
+      <li key={u.id} style={inlineStyle} onClick={() => this.selectUser(u.id)}>
+        {u.firstName} {u.lastName}
+      </li>
+    );
+  };
 
   render() {
-     const usersElements = this.state.users.map((user,id)=><li key={id}>{user}</li>)
-    console.log(usersElements);
-    return <ul>{usersElements}</ul>;
+    const { users } = this.state;
+    return <ul>{users.map(this.mapUsers)}</ul>;
   }
 }
 
