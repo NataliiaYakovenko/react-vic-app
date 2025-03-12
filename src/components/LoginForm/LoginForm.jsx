53a93cce0b6e2a8e028e3a5dc0a16b7aa77e10f1
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-12 15:26:13 +0200

    Add LoginForm валідація classNames

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import classNames from "classnames";
+import styles from "./LoginForm.module.css";
 
-const INITIAL_VALUE = {email: "", pasword: "",}
-
+const INITIAL_VALUE = { email: "", password: "" };
+const LOGIN_FORM_REX_EXP = {
+  email: /^.+@.+$/,
+  password: /^(?=.*[A-Z].*)(?=.*[a-z].*)(?=.*\d.*)(?=.*[!@#$%^&*.].*).{8,20}$/,
+};
 
 class LoginForm extends React.Component {
   constructor(props) {
@@ -9,55 +14,78 @@ class LoginForm extends React.Component {
 
     this.state = {
       email: INITIAL_VALUE.email,
-      pasword: INITIAL_VALUE.pasword,
+      isEmailValid: false,
+      password: INITIAL_VALUE.password,
+      isPasswordValid: false,
     };
   }
 
-  handleEmailChange = ({target:{value}}) => {
-    this.setState({ email: value });
+  handleEmailChange = ({ target: { value } }) => {
+    this.setState({
+      email: value,
+      isEmailValid: LOGIN_FORM_REX_EXP.email.test(value),
+    });
   };
-  handlePasswordChange=({target:{password}})=>{
-    this.setState({pasword: password})
-  }
-  handleSubmit=(e)=>{
-    e.preventDefault()
-  //відправляємо запит на сервер
-  this.setState(INITIAL_VALUE)  //- для скидування даних після відправки
-  }
-
+  handlePasswordChange = ({ target: { value } }) => {
+    this.setState({
+      password: value,
+      isPasswordValid: LOGIN_FORM_REX_EXP.password.test(value),
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    //відправляємо запит на сервер
+    this.setState(INITIAL_VALUE); //- для скидування даних після відправки
+  };
 
   render() {
-    const{email,pasword}= this.state
+    const { email, password, isEmailValid, isPasswordValid } = this.state;
+
+    //Приклад як писати вручну classnames
+    // const emailClassName = `${styles.input} ${isEmailValid ? styles.inputValid : styles.inputInValid }`
+    // console.log(emailClassName);
+
+    // Як потрібно писати classnames
+    const emailClassName = classNames(styles.input, {
+      [styles.inputValid]: isEmailValid,
+      [styles.inputInValid]: !isEmailValid,
+    });
+
+    const passwordClassName = classNames(styles.input, {
+      [styles.inputValid]: isPasswordValid,
+      [styles.inputInValid]: !isPasswordValid,
+    });
+
     return (
-      <>
-        <h1>LoginForm</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            <span>Email</span>
+      <div className={styles.formContainer}>
+        <h1 className={styles.formHeader}>LoginForm</h1>
+        <form className={styles.loginForm} onSubmit={this.handleSubmit}>
+          <label className={styles.formLabel}>
+            <span className={styles.inputName}>Email</span>
             <input
+              className={emailClassName}
               type="email"
               name="email"
-              placeholder="your email"
+              placeholder="your@email"
               value={email}
               onChange={this.handleEmailChange}
               autoFocus
             />
           </label>
-          <br/>
-          <label>
-            <span>Password</span>
+          <label className={styles.formLabel}>
+            <span className={styles.inputName}>Password</span>
             <input
+              className={passwordClassName}
               type="password"
               name="password"
               placeholder="your password"
-              value={pasword}
+              value={password}
               onChange={this.handlePasswordChange}
             />
           </label>
-          <br/>
           <button type="submit">Login</button>
         </form>
-      </>
+      </div>
     );
   }
 }
