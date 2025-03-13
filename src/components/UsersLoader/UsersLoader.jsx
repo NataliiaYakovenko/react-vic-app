c42bad473620a8a8481d615a2b6cad8c6144a11d
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-13 22:15:03 +0200

    Add перехід на сторінку вперед і на сторінку назад

--- src/components/UsersLoader/UsersLoader.jsx
+++ src/components/UsersLoader/UsersLoader.jsx
@@ -5,16 +5,14 @@ class UsersLoader extends React.Component {
     super(props);
 
     this.state = {
-      users: [
-        { id: 1, name: "Nataliia" },
-        { id: 2, name: "Roland" },
-      ],
+      users: [],
       isFetching: false,
       error: null,
       currentPage: 1,
     };
   }
-  componentDidMount() {
+
+  loading = () => {
     const { currentPage } = this.state;
 
     this.setState({ isFetching: true });
@@ -23,40 +21,48 @@ class UsersLoader extends React.Component {
       .then((data) => this.setState({ users: data.results, isFetching: false }))
       .catch((err) => this.setState({ error: err }))
       .finally(() => this.setState({ isFetching: false }));
+  };
+
+  componentDidMount() {
+    this.loading();
   }
 
-componentDidUpdate(prevProps, prevState){
-  const { currentPage } = this.state;
+  componentDidUpdate(prevProps, prevState) {
+    const { currentPage } = this.state;
 
-  if(currentPage !== prevState.currentPage){
-    this.setState({ isFetching: true });
-    fetch(`https://randomuser.me/api?results=5&seed=pe2022&page=${currentPage}`)
-      .then((response) => response.json())
-      .then((data) => this.setState({ users: data.results, isFetching: false }))
-      .catch((err) => this.setState({ error: err }))
-      .finally(() => this.setState({ isFetching: false }));
+    if (currentPage !== prevState.currentPage) {
+      this.loading();
+    }
   }
-}
 
-  nextPage=()=>{
-    const{currentPage}=this.state
-    return this.setState({currentPage:currentPage +1})
-  }
+  prevPage = () => {
+    const { currentPage } = this.state;
+    if (currentPage > 1) {
+      this.setState({ currentPage: currentPage - 1 });
+    }
+  };
+
+  nextPage = () => {
+    const { currentPage } = this.state;
+    this.setState({ currentPage: currentPage + 1 });
+  };
 
   render() {
     const { users, isFetching, error } = this.state;
     return (
       <>
-        <button>{"<"}</button>
-        <button onClick={this.nextPage}>{">"}</button>
         {error && <div>!!!ERROR!!!</div>}
         {isFetching && <div>Loading. Please wait</div>}
         {!error && !isFetching && (
-          <ul>
-            {users.map((user) => {
-              return <li key={user.id}>{JSON.stringify(user)}</li>;
-            })}
-          </ul>
+          <>
+            <button onClick={this.prevPage}>{"<"}</button>
+            <button onClick={this.nextPage}>{">"}</button>
+            <ul>
+              {users.map((user) => {
+                return <li key={user.id}>{JSON.stringify(user)}</li>;
+              })}
+            </ul>
+          </>
         )}
       </>
     );
