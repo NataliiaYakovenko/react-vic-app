3bf3c358001d4704d1fa2ac09373258203555b52
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-17 23:33:38 +0200

    Add Calendar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import ImageWrapper from "./components/ImageWrapper/ImageWrapper";
+import Calendar from "./components/Calendar/Calendar";
+
+ 
 
 function App() {
+
   return (
-    <ImageWrapper 
-    // width="500px" 
-    // height="400px" 
-    title="image">
-      <img
-        src="https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x2.jpg"
-        alt="It is cat"
-      />
-    </ImageWrapper>
-  );
+  <Calendar />
+  )
 }
 
 export default App;
