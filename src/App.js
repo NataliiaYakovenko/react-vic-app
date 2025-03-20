60a15206a60ee056baf8ebf82c888454198fa266
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-20 12:15:15 +0200

    Add render props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import Calendar from "./components/Calendar/Calendar";
+import DataProvider from "./components/DataProvider/DataProvider";
 
- 
+const PhonesLoadedList = (props) => {
+  const { data: phones, isFetching, error } = props.phonesData;
+  return (
+    <>
+      {isFetching && <div>Loading...</div>}
+      {error && <div>ERROR</div>}
+      <ol>
+        {phones.map((p) => (
+          <li key={p.id}>
+            {p.brand} {p.model}: {p.price}
+          </li>
+        ))}
+      </ol>
+    </>
+  );
+};
 
 function App() {
-
   return (
-  <Calendar />
+    // <DataProvider
+    //   fileName="/data/phones.json"
+    //   render={(data) => <PhonesLoadedList phonesData={data}/>}
+    // />
+   <DataProvider fileName="/data/phones.json">{
+    (data) => <PhonesLoadedList phonesData={data}/>
+
+   }</DataProvider>
+
+    
   )
 }
 
