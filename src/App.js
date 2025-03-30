e370b666f857f5c5ef4f8afa86f6af14c1f1df0a
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-30 16:10:10 +0300

    Add React Router 6 varios

--- src/App.js
+++ src/App.js
@@ -1,59 +1,30 @@
-import { useEffect } from "react";
 import {
-  BrowserRouter as Router,
   Link,
-  Switch,
   Route,
-  useHistory,
+  BrowserRouter as Router,
+  Routes,
+  useNavigate,
 } from "react-router-dom";
-import Counter from "./components/Counter/index";
-import StopWatch from "./components/StopWatch/StopWatch";
-import { useRouteMatch } from "react-router-dom/cjs/react-router-dom";
+import { useEffect } from "react";
 
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/components">Components</Link>
-          </li>
-          <li>
-            <Link to="/contacts">Contacts</Link>
-          </li>
-        </ul>
-      </nav>
+      <Link to="/">Home</Link>
+      <br />
+      <Link to="/about">About</Link>
 
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-
-        <Route path="/about">
-          <About />
-        </Route>
-
-        <Route path="/components">
-          <Components />
-        </Route>
-
-        <Route path="/contacts">
-          <Contacts />
-        </Route>
-
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
     </Router>
   );
 }
+//Link змінює location
+//Ruter підписаний на зміну location
+//Коли location змінюється, то Router шукає відповідний Route і виводе з нього елемент
 
 export default App;
 
@@ -65,48 +36,21 @@ function About() {
   return <div>About</div>;
 }
 
-//components/counter
-//components/stopwatch
-function Components() {
-  const { path, url } = useRouteMatch();
+function NotFound() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const id = setTimeout(() => navigate("/"), 4000);
+    return () => {
+      clearTimeout(id);
+    };
+  });
 
   return (
     <div>
-      <ol>
-        <li>
-          <Link to={`${url}/counter`}>Counter</Link>
-        </li>
-        <li>
-          <Link to={`${url}/stopwatch`}>StopWatch</Link>
-        </li>
-
-        <Switch>
-          <Route path={`${path}/counter`}>
-            <Counter />
-          </Route>
-
-          <Route path={`${path}/stopwatch`}>
-            <StopWatch />
-          </Route>
-
-          <Route path={`${path}/*`}>
-            <NotFound />
-          </Route>
-        </Switch>
-      </ol>
+      404 NotFound
+      <div>
+        <Link to="/">Home</Link>
+      </div>
     </div>
   );
-}
-
-function Contacts() {
-  return <div>Contacts</div>;
-}
-
-function NotFound() {
-  const history = useHistory();
-  useEffect(() => {
-    setTimeout(() => history.push("/"), 4000);
-  });
-
-  return <div>404 NotFound</div>;
 }
