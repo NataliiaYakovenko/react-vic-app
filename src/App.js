565e8a514d56758c45eb5c3c087134c9f6b30663
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-04-20 10:22:45 +0300

    Add Custom Huk

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import ContactPage from './pages/ContactPage/ContactPage';
+import UserLoaderHuk from './components/UserLoaderHuk/UserLoaderHuk'
 
 
 // HOC - компонент вищого порядку (функція, яка приймає і повертає компонент)
 function App() {
-  return <ContactPage />;
+  return <UserLoaderHuk />;
 }
 
 export default App;
--- src/components/UserLoaderHuk/UserLoaderHuk.jsx
+++ src/components/UserLoaderHuk/UserLoaderHuk.jsx
@@ -1,29 +1,37 @@
 
  import React, { useEffect, useState } from 'react';
 
+ function useData({currentPage, results}){
+  const [data, setData] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(null);
+
+ 
+  const loadData = () => {
+    setIsFetching(true);
+
+    fetch(
+      `https://randomuser.me/api?page=${currentPage}&results=${results}&seed=pe2022`
+    )
+      .then(response => response.json())
+      .then(data => setData(data.results))
+      .catch(e => setError(e))
+      .finally(() => setIsFetching(false));
+  };
+
+  useEffect(() => {
+    loadData();
+  }, [currentPage, results]);
+    return {data, isFetching,error}
+
+ }
+
  
  function UsersLoaderHuk() {
-   const [users, setUsers] = useState([]);
-   const [isFetching, setIsFetching] = useState(false);
-   const [error, setError] = useState(null);
    const [currentPage, setCurrentPage] = useState(1);
    const [results, setResults] = useState(5);
- 
-   const loadUsers = () => {
-     setIsFetching(true);
-     // getUsers({ page: currentPage, results: 5 })
-     fetch(
-       `https://randomuser.me/api?page=${currentPage}&results=${results}&seed=pe2022`
-     )
-       .then(response => response.json())
-       .then(data => setUsers(data.results))
-       .catch(e => setError(e))
-       .finally(() => setIsFetching(false));
-   };
- 
-   useEffect(() => {
-     loadUsers();
-   }, [currentPage, results]);
+   
+   const {data: users, isFetching,error}= useData({currentPage,results})
  
    const prevPage = () => {
      if (currentPage > 1) {
@@ -33,11 +41,6 @@
  
    const nextPage = () => {
      setCurrentPage(currentPage => currentPage + 1);
-     // setValue(10)
-     // setValue(function (value) {
-     //   return value + 1;
-     // });
-     // setValue(value=>value + 1);
    };
  
    return (
